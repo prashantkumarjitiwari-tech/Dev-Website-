99794f67e2bcc9c69d6e92045e8a33a34980013b
prashantkumarjitiwari-tech <prashantkumarjitiwari@gmail.com>
2026-08-07 16:23:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+});
+
+
+
+// Scroll animation
+
+const cards = document.querySelectorAll(".card, .box");
+
+
+window.addEventListener("scroll", ()=>{
+
+
+cards.forEach(card=>{
+
+
+let position = card.getBoundingClientRect().top;
+
+
+if(position < window.innerHeight - 100){
+
+card.style.opacity="1";
+card.style.transform="translateY(0)";
+
+
+}
+
+
+});
+
+
+});
+
+
+
+// Initial animation
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(40px)";
+card.style.transition="0.6s";
+
+});
